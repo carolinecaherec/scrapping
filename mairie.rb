c8033cde1480917bbfa3041fd287cd7fbcb7e1cc
carolinec <cahereccarolyn@hotmail.com>
2018-05-12 16:17:40 +0200

    V3

--- mairie.rb
+++ mairie.rb
@@ -1,7 +1,16 @@
-def mail_mairie1
-  body > div > main > section:nth-child(2) > div > table > tbody > tr:nth-child(4) > td:nth-child(2)
-  end
+require 'nokogiri'
+require 'open-uri'
 
-def url_mairies
+def mails_startup
+doc = Nokogiri::HTML(open('https://www.usine-digitale.fr/annuaire-start-up'))
+
+i =0
+   doc.css("annuaireStartUp > div.contenuPage > article > section:nth-child(5) > a").each do |email|
+    puts "Adresse email: #{email.content}"
+
+    i+=1
+  end
+end
+get_the_email_of_a_townhal_from_its_webpage()
 
   end
